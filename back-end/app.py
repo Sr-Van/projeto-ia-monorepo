53c2b60969d7f82186fc9ef5773f66162548f50b
Sr-Van <vanderson.dev1@gmail.com>
2026-01-15 17:49:37 -0300

    feat: metodo para analisar arquivo pdf e classifica-lo

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from services.ai_service import classify_emails
-
+from services.data_service import is_pdf_long, extract_text_from_pdf
 app = Flask(__name__)
 
 #Primeira rota apenas para teste
@@ -15,6 +15,23 @@ def analyze_email():
   result = classify_emails(email)
   return jsonify(result), 200
 
+@app.route('/analyze/pdf', methods=['POST'])
+def analyze_email_pdf():
+  if 'file' not in request.files:
+    return jsonify({'error': 'Nenhum arquivo enviado'}), 400
+
+  file = request.files['file']
+  if file.filename == '':
+            return 'Nenhum arquivo selecionado'
+  
+  if file and file.filename.endswith('.pdf'):
+     if is_pdf_long(file.stream):
+        return jsonify({'error': 'Arquivo PDF muito grande'}), 400
+     text = extract_text_from_pdf(file.stream)
+     result = classify_emails(text)
+     return jsonify(result), 200
+
+  return jsonify({'error': 'Formato de arquivo inválido'}), 400
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000, host='0.0.0.0')
