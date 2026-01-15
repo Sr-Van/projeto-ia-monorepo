a62e711d20620f3f1447d99f417dda8f09a49156
Sr-Van <vanderson.dev1@gmail.com>
2026-01-15 17:27:49 -0300

    feat: primeiro metodo POST para analizar os emails recebidos por texto com JSON

--- back-end/app.py
+++ back-end/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from services.ai_service import classify_emails
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.route('/teste', methods=['GET'])
 def hello_world():
   return jsonify({'message': 'Hello, World!'}), 200
+
+@app.route('/analyze', methods=['POST'])
+def analyze_email():
+  email = request.json['email']
+  
+  result = classify_emails(email)
+  return jsonify(result), 200
+
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000, host='0.0.0.0')
