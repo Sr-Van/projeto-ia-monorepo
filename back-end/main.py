d0de8c2250fc7d37d82ffe57a46be623445e00b2
Sr-Van <vanderson.dev1@gmail.com>
2026-01-14 21:39:45 -0300

    feat: usando gemini para testar as primeiras quotes e estabelecer conexao com a api

--- back-end/main.py
+++ back-end/main.py
@@ -1,3 +1,13 @@
-from openai import OpenAI
+from google import genai
+import os
+from dotenv import load_dotenv
 
-openai = OpenAI()
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
