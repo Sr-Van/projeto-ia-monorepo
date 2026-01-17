1b89976de20f16b9dd632147cdf614f46e746bdb
Sr-Van <vanderson.dev1@gmail.com>
2026-01-17 14:28:56 -0300

    feat: lidando com o input de arquivo para mostrar qual arquivo foi enviado.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,3 +7,27 @@ const cloneFile = templateFile.content.cloneNode(true);
 // dando append so para verificar o layout do template
 contTemplate.append(cloneEmail);
 contTemplate.append(cloneFile);
+const handleInputChange = (e) => {
+  const file = e.target.files[0];
+  const fileSelected = document.querySelector(".file-selected");
+  const labelInput = document.querySelector("#label-input");
+
+  // pequena animação para ficar mais fluido a remoção de um e aparição de outro
+  labelInput.classList.add("opacity-0", "h-0");
+  setTimeout(() => {
+    labelInput.classList.add("hidden");
+    fileSelected.classList.remove("hidden");
+  }, 350);
+
+  let span = document.createElement("span");
+  span.classList.add(
+    "text-sm",
+    "font-medium",
+    "text-emerald-700",
+    "text-center",
+    "px-4",
+  );
+
+  span.textContent = `Arquivo selecionado: ${file.name}`;
+  fileSelected.append(span);
+};
