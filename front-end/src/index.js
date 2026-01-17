2a0457e46517b711afcca823c9777eb699c698d9
Sr-Van <vanderson.dev1@gmail.com>
2026-01-17 17:47:11 -0300

    feat: ouvindo botoes para manipular template

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,7 @@
 const templateEmail = document.querySelector("#template-email");
 const templateFile = document.querySelector("#template-file");
 const contTemplate = document.querySelector("#container-template");
-
-const cloneEmail = templateEmail.content.cloneNode(true);
-const cloneFile = templateFile.content.cloneNode(true);
-
-// dando append so para verificar o layout do template
-contTemplate.append(cloneEmail);
-//contTemplate.append(cloneFile);
+const formElement = document.querySelector("#form-email");
 
 // deixando a api publica pois vou limitar as requisicoes e colocar o deploy na whitelist no back-end
 const api_url = "https://back-igl23m2j6-srvans-projects.vercel.app/analyze";
@@ -36,6 +30,20 @@ const handleInputChange = (e) => {
   fileSelected.append(span);
 };
 
+document.getElementById("button-email").addEventListener("click", () => {
+  const cloneEmail = templateEmail.content.cloneNode(true);
+  contTemplate.innerHTML = "";
+  contTemplate.append(cloneEmail);
+  formElement.classList.remove("hidden");
+});
+
+document.getElementById("button-file").addEventListener("click", () => {
+  const cloneFile = templateFile.content.cloneNode(true);
+  contTemplate.innerHTML = "";
+  contTemplate.append(cloneFile);
+
+  formElement.classList.remove("hidden");
+});
 const handleSubtmit = (e, form) => {
   e.preventDefault();
   const formData = new FormData(form);
